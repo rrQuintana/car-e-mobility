49a2ac36c9e915132835c7005446ec7d513d252c
Roberto Quintana <rrquintana7@gmail.com>
2024-07-23 22:48:31 -0600

    temp

--- car-e-mobility/src/app/shared/NavBar.jsx
+++ car-e-mobility/src/app/shared/NavBar.jsx
@@ -1,35 +1,43 @@
+import CarELogo from '../../assets/images/logo-small.png'
+
 function NavBar() {
   return (
-
-
-    <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+    <nav className="bg-white/90 backdrop-blur-md fixed w-full z-20 top-0 start-0 shadow-zinc-400/10 ease-in-out duration-300" id='NavBar'>
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto py-2 px-12">
+        <a href="/" className="flex items-center space-x-1 rtl:space-x-reverse">
+          <img src={CarELogo} className="h-12" alt="Car-e Logo" />
+          <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Car-e Mobility</span>
         </a>
         <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-          <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button>
-          <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
+          <button
+            type="button"
+            className="hidden md:block bg-white border-[0.9px] hover:bg-zinc-100 duration-300 ease-in-out border-zinc-800 rounded-full px-6 py-[10px] text-sm font-medium"
+          >
+            Pre-registro
+          </button>
+          <buttona
+            type="button"
+            aria-expanded="false"
+            aria-controls="navbar-sticky"
+            data-collapse-toggle="navbar-sticky"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          >
             <span className="sr-only">Open main menu</span>
             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
               <path stroke="currentColor" d="M1 1h15M1 7h15M1 13h15" />
             </svg>
-          </button>
+          </buttona>
         </div>
         <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul className="flex flex-col md:flex-row space-y-3 md:space-y-0 items-center space-x-0 md:space-x-2">
             <li>
-              <a href="#" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</a>
+              <a href="#" className="block py-2 px-3 text-sm" aria-current="page">Inicio</a>
             </li>
             <li>
-              <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+              <a href="#" className="block py-2 px-3 text-sm" aria-current="page">Nosotros</a>
             </li>
             <li>
-              <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
-            </li>
-            <li>
-              <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+              <a href="#" className="block py-2 px-3 text-sm" aria-current="page">Contacto</a>
             </li>
           </ul>
         </div>
